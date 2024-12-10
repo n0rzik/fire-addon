f1fb7a5901b5599fb89c3fe11d134a41236a7d9e
n0rzik <124731778+n0rzik@users.noreply.github.com>
2024-12-10 20:11:12 +0300

    Update PistonPush.java

--- src/main/java/n0rzik/fire/modules/combat/PistonPush.java
+++ src/main/java/n0rzik/fire/modules/combat/PistonPush.java
@@ -13,27 +13,7 @@ import java.util.List;
 
 public class PistonPush extends Module {
     public PistonPush () {
-        super(Fire.FIRE, "PistonPush", "does not work");
+        super(Fire.FIRE, "PistonPush", "No COMBECK");
     }
-    private final SettingGroup sgGenetal = settings.getDefaultGroup();
-
-    private final Setting<Double> range = sgGenetal.add(new DoubleSetting.Builder()
-            .name("Range")
-            .defaultValue(1)
-            .sliderRange(1,3)
-            .build()
-    );
-
-    private List<BlockPos> queue = new ArrayList<>();
-
-    public final List<BlockPos> pistonRow = List.of(new BlockPos(0, 1, 1), new BlockPos(0, 1, 1), new BlockPos(-1, 1, 0) , new BlockPos(0, 1, -1));
-
-    public void onPushTick(TickEvent.Pre event) {
-        boolean fakePlayer = false;
-    }
-    //piston push does not work!!!
-    //piston push does not work!!!
-    //piston push does not work!!!
-    //piston push does not work!!!
-    //piston push does not work!!!
+    //n0rzik no aboudno
 }
